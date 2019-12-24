ceeafa8dafc74286c73e8297de8af19e6e297e2c
theArgex <eragonder@gmail.com>
2019-12-24 13:22:38 +0100

    add nebular particles and first test layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParticlesModule } from 'ng-particle';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'new-tab';
+  myStyle: object = {};
+  myParams: object = {};
+  width = 100;
+  height = 100;
+
+  ngOnInit() {
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+    };
+
+    this.myParams = {
+      particles: {
+        number: {
+          value: 100,
+        },
+        color: {
+        },
+        line_linked: {
+          enable: true,
+          color: '#ffffff',
+          opacity: 0.4,
+          width: 2
+        },
+        shape: {
+          type: 'triangle',
+        },
+      }
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbSearchModule, NbInputModule, NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ParticlesModule } from 'angular-particle';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    ParticlesModule,
+    AppRoutingModule,
+    NbInputModule,
+    NbCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
